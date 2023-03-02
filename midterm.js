1ba3b93b885be7ec0a5cd73fc97f080afb042fcd
LinhLuu254 <llinh254@gmail.com>
2023-03-02 18:06:30 -0500

    updated files

--- midterm.js
+++ midterm.js
@@ -1,53 +1,63 @@
 const cakes =[
     {
-        cakeName: "Lemon-Ice",
+        cakeName: "Lemon Ice",
         calories: 120,
         price: 3.5,
+        pic: "images\\Lemon.jpg"
     },
     {
-        cakeName: "Choco-Truffle",
+        cakeName: "Choco Truffle",
         calories: 230,
         price: 5,
+        pic: "images\\Choco.jpg"
     },
     {
-        cakeName: "Vanilla-Truffle",
+        cakeName: "Vanilla Truffle",
         calories: 220,
         price: 4.5,
+        pic: "images\\Vanilla.jpg"
     },
     {
-        cakeName: "Orange-Zet",
+        cakeName: "Orange Zest",
         calories: 230,
         price: 5,
+        pic: "images\\Orange.jpg"
     },
     {
-        cakeName: "Penut-Butter",
+        cakeName: "Peanut Butter",
         calories: 320,
         price: 4.5,
+        pic: "images\\Peanut-Butter.jpg"
     },
     {
-        cakeName: "Oreo-Truffle",
+        cakeName: "Oreo Truffle",
         calories: 330,
         price: 5.5,
+        pic: "images\\oreo-truffles.jpg"
     },
     {
         cakeName: "Carrot Cake",
         calories: 240,
         price: 3.5,
+        pic: "images\\Carrot.jpg"
     },
     {
         cakeName: "Apple Pie",
         calories: 260,
         price: 5.5,
+        pic: "images\\Apple-Pie.jpg"
     },
     {
         cakeName: "Beer Cake",
         calories: 320,
         price: 6,
+        pic: "images\\Beer_Cake.jpg"
     },
     {
         cakeName: "Brownie",
         calories: 210,
         price: 3.5,
+        pic: "images\\brownie.jpg"
     },
 ];
 
@@ -77,15 +87,20 @@ const showInfo = () =>{
 
 
 const listCakes = () =>{
+
     let cakesList = document.querySelector(".cakesList");
-    let allcakesList = `<ol>`;
+    let allcakesList = `<ul>`;
+    
     cakes.forEach((cake) => {
-        allcakesList += `<li> ${cake.cakeName}</li>`;
+        allcakesList += `<li> ${cake.cakeName}</li><br>`;
+          
     });
-    allcakesList += `</ol>`;
+    allcakesList += `</ul>`;
     
     cakesList.innerHTML = allcakesList;
+    
 
+   
 }
 const createCake = () =>{
     let name = prompt ("Enter the cake's name:");
@@ -96,6 +111,7 @@ const createCake = () =>{
         cakeName: name,
         calories: parseFloat(cals),
         price: parseFloat(price),
+        pic: "cake-decorations.jpg"
     });
 
     listCakes();
@@ -103,16 +119,19 @@ const createCake = () =>{
 }
 
 const readCake = () =>{
-    let respone = prompt ("Which cake do you want to view?: ");
-    let display = " "
+    let response = prompt("Enter the cake that you want to view:")
 
-    cakes.find((cake) => {
-        if (cake.cakeName == respone)
-        display = `${cake.cakeName} has ${cake.calories} calories. Price: $${cake.price}.`
+    let index = cakes.findIndex((cake) => cake.cakeName === response)
 
+        display = `${cakes[index].cakeName} has ${cakes[index].calories} calories. Price: $${cakes[index].price}!`
+        
         let cakeDisplay = document.querySelector(".selectedTask");
         cakeDisplay.innerHTML = display;
-    }) ;    
+
+        let cakePic = document.querySelector(".cakesPic");
+        // set the image
+        cakePic.src = cakes[index].pic;
+    
 
 }
 
@@ -127,9 +146,11 @@ const updateCake = () => {
         cakeName: name,
         calories: parseFloat(cals),
         price: parseFloat(price),
+        pic: "cake-decorations.jpg"
     }
 
-    listCakes();   
+    listCakes();
+    console.log(cakes);  
 };
 
 const deleteCake = () =>{
@@ -142,6 +163,7 @@ const deleteCake = () =>{
     listCakes(); 
 
     console.log (deleteCake);
+    console.log(cakes);
 
 }
 
